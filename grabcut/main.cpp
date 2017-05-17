f5f47b7fdaaacc4cf596f4fa153eeceb6e625e74
smile547 <karin.ng@mail.utoronto.ca>
2017-05-17 11:44:42 -0400

    Correcting intialization and testing energy (problem =/)

--- grabcut/main.cpp
+++ grabcut/main.cpp
@@ -12,9 +12,9 @@
 #define NUM_CLUS_BG 2 // Number of clusters for bg GMM Estimation
 #define LAMBDA 5 // Graph weight parameter
 #define KEEP 10000000 // Thick edge weight
-#define ITER 2
+#define ITER 4
 
-typedef Graph<int, int, int> GraphType;
+typedef Graph<double, double, double> GraphType;
 double beta;
 cv::String fileBase = "../samples/noise_blur_12";
 
@@ -77,34 +77,53 @@ int getMask(cv::String& fn, cv::Mat& img, cv::Mat& mask)
 /*
 Given the image and mask, change points into a 1D matrix to be passed for GMM training
 */
-void estGMM(cv::Mat& mask, cv::Mat& img, cv::Mat& likelihoods, int num_clus)
+void estGMM(cv::Mat& mask, cv::Mat& img, int num_clus)
 {
 	// Get points
-	cv::Mat points = cv::Mat::zeros(cv::countNonZero(mask), 1, CV_32SC1);
+	cv::Mat sample_points = cv::Mat::zeros(cv::countNonZero(mask), 1, CV_32SC1);
 	int ctr = 0;
 	for (int i = 0; i < mask.rows; i++) {
 		for (int j = 0; j < mask.cols; j++) {
-			if (mask.at<uchar>(i, j) > 0) {
-				points.at<int>(ctr) = img.at<uchar>(i, j);
+			if (mask.at<double>(i, j) > 0) {
+				sample_points.at<int>(ctr) = img.at<uchar>(i, j);
 				ctr++;
 			}
 		}
 	}
 
+	cv::Mat likelihoods;
 	cv::Ptr<cv::ml::EM> mdl = cv::ml::EM::create();
 	mdl->setClustersNumber(num_clus);
-	mdl->trainEM(points, likelihoods);
+	mdl->trainEM(sample_points, likelihoods);
+
+	// Set the likelihoods to the masks
+	ctr = 0;
+	for (int i = 0; i < mask.rows; i++) {
+		for (int j = 0; j < mask.cols; j++) {
+			if (mask.at<double>(i, j) > 0) {
+				mask.at<double>(i, j) = likelihoods.at<double>(ctr);
+				ctr++;
+			}
+			else { // Predict rest of points (not in mask)
+				cv::Mat pt = cv::Mat::zeros(1, 1, CV_32SC1); 
+				pt.at<int>(0) = img.at<uchar>(i, j);
+				cv::Vec2d point_likelihood;
+				point_likelihood = mdl->predict2(pt, cv::noArray());
+				mask.at<double>(i, j) = point_likelihood(0);
+			}
+		}
+	}
 }
+
 
 /*
 Create the graph for foreground/background segmentation and run cut
 */
 void createGraph(GraphType *g, cv::Mat& img, cv::Mat& fg_seed, cv::Mat& bg_seed, 
-				cv::Mat& fg_mask, cv::Mat& bg_mask, cv::Mat& fg_prob, cv::Mat& bg_prob)
+				cv::Mat& fg_mask, cv::Mat& bg_mask)
 {
 	int nrows = img.rows;
 	int ncols = img.cols;
-	double energy = 0;
 
 	// Create the graph
 	// Add the nodes
@@ -117,7 +136,6 @@ void createGraph(GraphType *g, cv::Mat& img, cv::Mat& fg_seed, cv::Mat& bg_seed,
 			int diff = abs((int)img.at<uchar>(i - 1, j) - (int)img.at<uchar>(i, j));
 			double weight = diff > 0 ? LAMBDA*exp(-beta*(diff*diff)) : KEEP;
 			//std::cout << "weight: " << weight << std::endl;
-			energy += 2*weight;
 			g->add_edge((i - 1)*ncols + j, i*ncols + j, weight, weight);
 		}
 	}
@@ -126,47 +144,53 @@ void createGraph(GraphType *g, cv::Mat& img, cv::Mat& fg_seed, cv::Mat& bg_seed,
 			int diff = abs((int)img.at<uchar>(i, j - 1) - (int)img.at<uchar>(i, j));
 			double weight = diff > 0 ? LAMBDA*exp(-beta*(diff*diff)) : KEEP;
 			g->add_edge(i*ncols + (j - 1), i*ncols + j, weight, weight);
-			energy += 2*weight;
 		}
 	}
 	// Add t-links - changes depending on the mask
-	int pidx = 0;
 	for (int i = 0; i < nrows; i++) {
 		for (int j = 0; j < ncols; j++) {
 			// Make sure seeded values won't be cut
 			if (fg_seed.at<uchar>(i, j) > 0) {
-				g->add_tweights(i*ncols + j, KEEP, 0);
-				energy += KEEP;
+				g->add_tweights(i*ncols + j, 0, KEEP);
 			} 
-			else if (fg_mask.at<uchar>(i, j) > 0) {
+			else {
 				//std::cout << "fg: " << -fg_prob.at<double>(pidx) << std::endl;
-				g->add_tweights(i*ncols + j, -fg_prob.at<double>(pidx), 0);
-				energy += -fg_prob.at<double>(pidx);
+				g->add_tweights(i*ncols + j, -fg_mask.at<double>(i, j), 0);
 			}
-			if (fg_mask.at<uchar>(i, j) > 0)
-				pidx++;
 		}
 	}
-	pidx = 0;
 	for (int i = 0; i < nrows; i++) {
 		for (int j = 0; j < ncols; j++) {
 			// Make sure seeded values won't be cut
 			if (bg_seed.at<uchar>(i, j) > 0) {
-				g->add_tweights(i*ncols + j, 0, KEEP);
-				energy += KEEP;
+				g->add_tweights(i*ncols + j, KEEP, 0);
 			}
-			else if (bg_mask.at<uchar>(i, j) > 0) {
-				g->add_tweights(i*ncols + j, 0, -bg_prob.at<double>(pidx));
-				energy += -bg_prob.at<double>(pidx);
+			else {
+				g->add_tweights(i*ncols + j, 0, -bg_mask.at<double>(i, j));
 			}
-			if (bg_mask.at<uchar>(i, j) > 0)
-				pidx++;
 		}
 	}
 
 	double flow = g->maxflow();
 	std::cout.precision(17);
-	std::cout << "Flow is: " << flow << ", Energy is: " << energy << std::endl;
+	std::cout << "Flow is: " << flow << std::endl;
+}
+
+void getEnergy(cv::Mat& fg_mask, cv::Mat& fg_prob, cv::Mat& bg_mask, cv::Mat& bg_prob)
+{
+	int nrows = fg_mask.rows;
+	int ncols = fg_mask.cols;
+	double energy = 0;
+
+	for (int i = 0; i < nrows; i++) {
+		for (int j = 0; j < ncols; j++) {
+			if (fg_mask.at<double>(i, j) > 0)
+				energy += -fg_prob.at<double>(i, j);
+			else 
+				energy += -bg_prob.at<double>(i, j);
+		}
+	}
+	std::cout << "GMM Energy is: " << energy << std::endl;
 }
 
 
@@ -186,37 +210,42 @@ int main(int argc, char** argv)
 
 	// Foreground data
 	cv::String fgName = fileBase+"_fg.bmp";
-	cv::Mat fg_mask, fg_likelihoods;
+	cv::Mat fg_mask;
 	getMask(fgName, img, fg_mask);
 
 	// Background data
 	cv::String bgName = fileBase+"_bg.bmp";
-	cv::Mat bg_mask, bg_likelihoods;
+	cv::Mat bg_mask;
 	getMask(bgName, img, bg_mask);
 	
 	int iter = 0;
 	cv::Mat new_fg_mask = fg_mask.clone();
+	new_fg_mask.convertTo(new_fg_mask, CV_64FC1);
 	cv::Mat new_bg_mask = bg_mask.clone();
+	new_bg_mask.convertTo(new_bg_mask, CV_64FC1);
 
 	while (iter <= ITER) {
 		std::cout << "RUN #" << iter << "\n=========================\n";
 		std::cout << "Estimating GMMs\n";
-		estGMM(new_fg_mask, img, fg_likelihoods, NUM_CLUS_FG);
-		estGMM(new_bg_mask, img, bg_likelihoods, NUM_CLUS_BG);
+		cv::Mat fg_prob = new_fg_mask.clone();
+		cv::Mat bg_prob = new_bg_mask.clone();
+		estGMM(fg_prob, img, NUM_CLUS_FG);
+		estGMM(bg_prob, img, NUM_CLUS_BG);
+		getEnergy(new_fg_mask, fg_prob, new_bg_mask, bg_prob);
 
 		std::cout << "Creating Graph\n";
-		createGraph(g, img, fg_mask, bg_mask, new_fg_mask, new_bg_mask, fg_likelihoods, bg_likelihoods);
+		createGraph(g, img, fg_mask, bg_mask, fg_prob, bg_prob);
 
 		// Reset the masks
 		std::cout << "Recalculating foreground/background\n\n";
-		new_fg_mask = cv::Mat::zeros(nrows, ncols, CV_8UC1);
-		new_bg_mask = cv::Mat::zeros(nrows, ncols, CV_8UC1);
+		new_fg_mask = cv::Mat::zeros(nrows, ncols, CV_64FC1);
+		new_bg_mask = cv::Mat::zeros(nrows, ncols, CV_64FC1);
 		for (int i = 0; i < nrows; i++) {
 			for (int j = 0; j < ncols; j++) {
 				if (g->what_segment(i*ncols + j) == GraphType::SOURCE)
-					new_fg_mask.at<uchar>(i, j) = 1;
+					new_fg_mask.at<double>(i, j) = 1;
 				else
-					new_bg_mask.at<uchar>(i, j) = 1;
+					new_bg_mask.at<double>(i, j) = 1;
 			}
 		}
 
@@ -228,7 +257,7 @@ int main(int argc, char** argv)
 	cv::Mat finalImg = cv::Mat::zeros(nrows, ncols, CV_8UC1);
 	for (int i = 0; i < nrows; i++)
 		for (int j = 0; j < ncols; j++)
-			finalImg.at<uchar>(i, j) = new_fg_mask.at<uchar>(i, j) ? 255 : 0;
+			finalImg.at<uchar>(i, j) = new_fg_mask.at<double>(i, j) ? 255 : 0;
 
 	// Display image
 	cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE); // Create a window for display.
